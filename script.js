67ed1d666b759127a79dc96fb38a4381ba437f0f
arsal0308 <arsal.imran0308@gmail.com>
2023-06-30 16:29:30 -0400

    Template

---
+++ script.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+// Identify and initialize state variables.
+// Code the main render(), renderScores() & renderResults() functions.
+// Code the click event listener, including the win logic.
+// Update the renderResults() function to render the winner border.
+// Code the countdown timer.
