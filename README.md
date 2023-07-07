6a5226b9089cb1ae9ac52e0a52cc9cce6b4daa82
arsal0308 <arsal.imran0308@gmail.com>
2023-07-07 11:44:08 -0400

    Updated Win/Loss Logic

--- README.md
+++ README.md
@@ -1,17 +1,4 @@
 # Spaceman
-Game Description:
-This is a single player game of Spaceman. The game is played by clicking the
-"Generate Random Word" button which will store a random word unknown to the player. 
-The player will be able to see how many letters the word consists of indicated by 
-the number of blank spaces. The player will be able to select a letter from A through Z as buttons on the screen. 
-For each correct guess of a letter the word contains, the
-blank space will be filled by that letter. Otherwise, if a guess is incorrect, the button to select
-the letter will be greyed out and part of the spaceship will appear. The player 
-will continue to select letters until either the word is correctly guessed in which 
-case the player wins, else the spaceship will fully appear indicating the player has lost. 
-Good luck!
-
-
 
 Pseudocode:
 
@@ -25,3 +12,40 @@ Pseudocode:
 9. Replace “_” with the matched letter, otherwise decrease attempts remaining by 1 for each incorrect guess
 10. If the string created matches the generated word, then display “YOU WIN!”
 11. Otherwise if attempts remaining = 0 display “YOU LOSE!” 
+
+
+
+# Spaceman
+This is a single player game of Spaceman. The game is played by clicking the
+"Generate Random Word" button which will store a random word unknown to the player. 
+The player will be able to see how many letters the word consists of indicated by 
+the number of blank spaces. The player will be able to select a letter from A through Z as buttons on the screen. 
+For each correct guess of a letter the word contains, the
+blank space will be filled by that letter. Otherwise, if a guess is incorrect, the button to select
+the letter will be greyed out and part of the spaceship will appear. The player 
+will continue to select letters until either the word is correctly guessed in which 
+case the player wins, else the spaceship will fully appear indicating the player has lost. 
+Good luck!
+
+# Screenshot
+
+<img src="url to your image on imgur"> open image in new tab then copy url
+<img src="url to your image on imgur">
+
+# Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+
+# Getting Started
+
+[Click to Play Spaceman!](https://github.com/arsal0308/Spaceman)
+
+# Next Steps
+
+- In the future, I would like to have parts of a stick figure appear so that it corresponds with the number of increasing 
+    incorrect guesses.
+- As well as, I would like to have different sounds play throughout the game. For example, sound playing at the loading screen
+    prior to selecting the "Generate Random Word" button, sound effects when selecting the letter buttons, and sounds for
+    either winning or losing the game. 
--- script.js
+++ script.js
@@ -17,12 +17,10 @@ document.body.appendChild(lettersContainer);
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
 /*----- app's state (variables) -----*/
 let randomWord = "";
 let guessedWord = [];
 let attemptsRemaining = 10;
-
 /*----- cached element references -----*/
 const randomWordEl = document.getElementById("randomWord");
 
@@ -36,76 +34,90 @@ resetButton.addEventListener("click", resetGame);
 // Code the click event listener, including the win logic.
 // Update the renderResults() function to render the winner border.
 
-
 // Player presses "New Game" button
 
 // % Prompt user to click generate random word
 function startNewGame() {
-    generateRandomWord();
-    renderLetters();
+  generateRandomWord();
+  renderLetters();
+  resultEl.textContent = "";
+  resultEl.classList.remove("win", "lose");
 }
 
+function generateRandomWord() {
+  const words = [
+    "boxful",
+    "lengths",
+    "jukebox",
+    "jumbo",
+    "snazzy",
+    "jinx",
+    "cobweb",
+    "kayak",
+    "zombie",
+    "juicy",
+  ];
 
-    function generateRandomWord() {
-        const words = ["boxful", "lengths", "jukebox", "jumbo", "snazzy", "jinx", "cobweb",
-        "kayak", "zombie", "juicy"]
+  const randomIndex = Math.floor(Math.random() * words.length);
+  randomWord = words[randomIndex].toUpperCase();
 
-        const randomIndex = Math.floor(Math.random() * words.length);
-        randomWord = words[randomIndex].toUpperCase();
+  // % Initiate an array of same length as word, but each character is an “_”.
+  guessedWord = Array(randomWord.length).fill("_");
 
-    // % Initiate an array of same length as word, but each character is an “_”.
-        guessedWord = Array(randomWord.length).fill("_");
+  // % Initialize variable attempts remaining = 10
 
-    // % Initialize variable attempts remaining = 10
-        
-        renderRandomWord();
-        }
+  renderRandomWord();
+}
 
-        function renderRandomWord() {
-            randomWordEl.textContent = guessedWord.join("");
-        }
-        function renderLetters () {
-            const letters = alphabet.split("");
-            lettersContainer.innerHTML = "";
+function renderRandomWord() {
+  randomWordEl.textContent = guessedWord.join("");
+}
+function renderLetters() {
+  const letters = alphabet.split("");
+  lettersContainer.innerHTML = "";
 
-            letters.forEach(letter => {
-                const button = document.createElement("button");
-                button.textContent = letter;
-                button.addEventListener("click", () => selectLetter(letter));
-                lettersContainer.appendChild(button);
-            });
-        }
+  letters.forEach((letter) => {
+    const button = document.createElement("button");
+    button.textContent = letter;
+    button.addEventListener("click", () => selectLetter(letter));
+    lettersContainer.appendChild(button);
+  });
+}
 
-        function selectLetter(letter) {
-            if (attemptsRemaining > 0 && !guessedWord.includes(letter)) {
-                let matched = false;
-                for (let i = 0; i < randomWord.length; i++) {
-                  
-                    if (randomWord[i] === letter) {
-                        guessedWord[i] = letter;
-                        matched = true;
-                    } 
-                }
-                if (!matched) {
-                    attemptsRemaining --;
-                }
-                renderRandomWord();
-                checkGameResult();
-            } 
-        }
+function selectLetter(letter) {
+  if (attemptsRemaining > 0 && !guessedWord.includes(letter)) {
+    let matched = false;
+    for (let i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] === letter) {
+        guessedWord[i] = letter;
+        matched = true;
+      }
+    }
+    if (!matched) {
+      attemptsRemaining--;
+    }
+    renderRandomWord();
+    checkGameResult();
+  }
+}
 
-        function checkGameResult() {
-            if (guessedWord.join("") === randomWord) {
-                console.log("YOU WIN!");
-            } else if (attemptsRemaining === 0) {
-                console.log("YOU LOSE!");
-            }
-        }
+function checkGameResult() {
+    if (guessedWord.join("") === randomWord) {
+      resultEl.textContent = "You Win!";
+      resultEl.classList.add("win");
+    } else if (attemptsRemaining === 0) {
+      resultEl.textContent = "You Lose!";
+      resultEl.classList.add("lose");
+    }
+  }
 
-        function resetGame() {
-            randomWord = "";
-            guessedWord = [];
-            attemptsRemaining = 10;
-            randomWordEl.textContent = "";
-            lettersContainer.innerHTML = "";
-          }
+function resetGame() {
+  resultEl.textContent = "";
+  resultEl.classList.remove("win", "lose");
+
+  randomWord = "";
+  guessedWord = [];
+  attemptsRemaining = 10;
+  randomWordEl.textContent = "";
+  lettersContainer.innerHTML = "";
+}
